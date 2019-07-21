4b74ff5a68a4de161d9b84d5c92839956eba9e26
MhdBashar <mhdbashard@gmail.com>
2019-07-21 14:13:26 +0300

    fix label management module

---
+++ application/language/english/custom_lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['Id'] = "ID";
+$lang['procuration'] = "Procuration";
+$lang['procurations'] = "Procurations";
+$lang['procuration_state'] = "Procuration state";
+$lang['procuration_type'] = "Procuration type";
+$lang['new_procuration_type'] = "New Procuration type";
+$lang['new_procuration_state'] = "New Procuration state";
+$lang['new_procuration'] = "New Procuration";
+$lang['company'] = "Company";
+$lang['individual'] = "Individual";
+$lang['customer_details'] = "Customer Name (Company or Individual)";
+$lang['customer_type'] = "Customer Type";
+$lang['customer_representative'] = "Customer Representative";
+$lang['new_customer_representative'] = "New Customer Representative";
+$lang['new_judge'] = "New Judge";
+$lang['case_status'] = "Case Status";
+$lang['new_cstatus'] = "New Case Status";

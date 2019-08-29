93c0956b213de46a3016a1dd8bf466aca8c00777
ahmadzaher <ahmed.khrezaty@gmail.com>
2019-08-29 12:06:27 +0300

    Fix Branch Module

---
+++ modules/branches/controllers/Branches.php
@@ -0,0 +1,147 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Branches extends AdminController
+{
+
+    private $pdf_fields = [];
+
+    private $client_portal_fields = [];
+
+ 
+    private $client_editable_fields = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('branches_model');
+
+
+        $this->load->model('branches_model');
+
+ 
+        if (!is_admin()) {
+            access_denied('Access Branches');
+        }
+        // Add the pdf allowed fields
+
+        $this->pdf_fields             = $this->branches_model->get_pdf_allowed_fields();
+        $this->client_portal_fields   = $this->branches_model->get_client_portal_allowed_fields();
+        $this->client_editable_fields = $this->branches_model->get_client_editable_fields();
+    }
+
+
+    /* List all properties */
+    public function index()
+    {
+/*        error_reporting(E_ALL);
+        ini_set('display_errors', 1);*/
+
+        if ($this->input->is_ajax_request()) {
+            $this->branchapp->get_table_data('branches');
+ 
+        }
+        $data['title'] = _l('branches');
+        $this->load->view('admin/branches/manage', $data);
+    }
+
+    public function field($id = '')
+    {
+        if ($this->input->post()) {
+            if ($id == '') {
+                $id = $this->branches_model->add($this->input->post());
+                if ($id) {
+                    set_alert('success', _l('added_successfully', _l('branch')));
+                    redirect(admin_url('branches/field/' . $id));
+                }
+            } else {
+                $success = $this->branches_model->update($this->input->post(), $id);
+                if (is_array($success) && isset($success['cant_change_option_custom_field'])) {
+                    set_alert('warning', _l('cf_option_in_use'));
+                } elseif ($success === true) {
+                    set_alert('success', _l('updated_successfully', _l('branch')));
+                }
+                redirect(admin_url('branches/field/' . $id));
+            }
+        }
+        if ($id == '') {
+            $title = _l('add_new', _l('branches_lowercase'));
+        } else {
+            $data['branch'] = $this->branches_model->get($id);
+            $title                = _l('edit', _l('branches_lowercase'));
+            $data['city']=$this->branches_model->getCitiesForCountry($data['branch']->country_id);
+        }
+        $data['pdf_fields']             = $this->pdf_fields;
+        $data['client_portal_fields']   = $this->client_portal_fields;
+        $data['client_editable_fields'] = $this->client_editable_fields;
+        $data['title']                  = $title;
+        $data['countries']=$this->branches_model->getCountries();
+        $this->load->view('admin/branches/branch', $data);
+    }
+
+
+    /**
+    *   Description: Delte Branch
+     *  @param: Id int
+    */
+
+    /* Delete announcement from database */
+ 
+    public function delete($id)
+    {
+        if (!$id) {
+            redirect(admin_url('branches'));
+        }
+        $response = $this->branches_model->delete($id);
+        if ($response == true) {
+            set_alert('success', _l('deleted', _l('branch')));
+        } else {
+            set_alert('warning', _l('problem_deleting', _l('branches_lowercase')));
+        }
+        redirect(admin_url('branches'));
+    }
+
+
+
+    /**
+     * @param $id
+     */
+
+ 
+    public function getCities($id)
+    {
+        echo json_encode(['success'=>true,'data'=>$this->branches_model->getCitiesForCountry($id)]);
+        die();
+    }
+
+
+
+    /**
+     * Descripion: retreive  departments for Branch
+     * @param $id
+     */
+    public function departments($id)
+    {
+    
+        /*echo $this->app_modules->is_active('branches');
+        die();*/
+        if ($this->input->is_ajax_request()) {
+            $this->app->get_table_data(module_views_path('branches', 'admin/tables/branch_departments'), ['id' => $id]);
+        }
+        
+        $data['email_exist_as_staff'] = $this->email_exist_as_staff();
+        $data['branches']=$this->branches_model->getBranches();
+        $data['title']                = _l('departments');    
+        $this->load->view('admin/branches/manage_department', $data);
+ 
+    }
+
+    private function email_exist_as_staff()
+    {
+        return total_rows('tbldepartments', 'email IN (SELECT email FROM tblstaff)') > 0;
+    }
+
+
+}
+ 
--- modules/hrm/controllers/Award.php
+++ modules/hrm/controllers/Award.php
@@ -1,85 +1,22 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
 
-
-class Award extends AdminController{
-	public function __construct(){
-        parent::__construct();
-        $this->load->model('awards');
-    }
-
-    public function index(){
-        if(!is_admin()){
-            access_denied();
-        }
-
-        if($this->input->is_ajax_request()){
-            $this->hrmapp->get_table_data('my_award_table');
-        }
-        $data['title'] = 'awards';
-        
-        $this->load->view('awards/award', $data);
-    }
-    public function get($id)
-    {
-        $data = $this->awards->getAward($id);
-        echo json_encode($data);
-    }
-
-
+/**
+ *  
+ */
+class Award extends AdminController
+{
     
-    public function update(){
-        if (!is_admin()) {
-            access_denied();
-        }
-        if ($this->input->get()) {
-            $data            = $this->input->get();
-            $id           = $this->input->get('id');
-            // $data['message'] = $this->input->post('message', false);
-            $success = $this->awards->update($data, $id);
-            if ($success) {
-                set_alert('success', _l('updated_successfully', 'Award'));
-            }
-            redirect('hrm/award');
-        }
-        if ($id == '')
-        redirect('hrm/award');
-    }    
-   
-    public function AddNew(){
-        if (!is_admin()) {
-            access_denied();
-        }
-        if ($this->input->get()) {
-            $data            = $this->input->get();
-            
-            $data['date'] = '20' . date('y') . '-' . date('m');
-            $id           = $this->input->get('id');
-            // $data['message'] = $this->input->post('message', false);
-            $success = $this->awards->update($data, $id);
-            if ($success) {
-                set_alert('success', 'Award Added Successfully');
-            }
-            redirect('hrm/award');
-        }
-        if ($id == '')
-        redirect('hrm/award');
-    }
-
-    public function delete($id)
-    {
-        if (!$id) {
-            access_denied();
-        }
-        if (!is_admin()) {
-            access_denied();
-        }
-        $response = $this->awards->delete($id);
-        if ($response == true) {
-            set_alert('success', _l('deleted', "Award"));
-        } else {
-            set_alert('warning', _l('problem_deleting', "Award"));
-        }
-        redirect('hrm/award');
+    function index(){
+        $_POST['fullname'] = "Ahmad Zaher";
+        $_POST['username'] = "ahmadzaher";
+        $_POST['password'] = "12345ahmad";
+        $_POST['email'] = "ahmadkhrezaty@gmail.com";
+        $_POST['locale'] = "Syria";
+        $_POST['language'] = "English";
+        $_POST['phone'] = "5319932";
+        $_POST['mobile'] = "0944543666";
+        $_POST['role_id'] = "admin";
+        $_POST['direction'] = "LTR";
+        var_dump($this->input->post());
     }
 }

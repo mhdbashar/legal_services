73c3da78e55fc41a6b89d78b6f255dc6bcbea7ff
Ahmad Zaher Khrezaty <ahmedkhrezaty1@gmail.com>
2019-11-04 03:32:06 +0200

    Fix Error In Deductions

--- modules/hr/views/details/salary/statutory_deductions.php
+++ modules/hr/views/details/salary/statutory_deductions.php
@@ -1,5 +1,4 @@
  <?php $this->load->view('details/modals/statutory_deduction_modal') ?>
- <?php $this->load->view('details/modals/deduction_type_modal') ?>
  <div class="_buttons">
     <a href="#" class="btn btn-info pull-left" data-toggle="modal" data-target="#add_statutory_deduction"><?php echo 'New Statutory Deductions'; ?></a>
 </div>
@@ -14,8 +13,3 @@
     'Amount',
     'Actions',
     ),'statutory_deductions'); ?>
-
-<div class="clearfix"></div>
-<hr class="hr-panel-heading" />
-<div class="clearfix"></div>
-	<a href="#" class="btn btn-info pull-left" data-toggle="modal" data-target="#add_deduction_type"><?php echo 'Add deduction type'; ?></a>

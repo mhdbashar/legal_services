5d1968ba6ba8d2b852d56cd4ca85939d2f2da432
mhdbashar <mhdbashard@gmail.com>
2019-12-07 10:53:06 +0200

    Add mobily gateway

--- application/helpers/my_functions_helper.php
+++ application/helpers/my_functions_helper.php
@@ -1,53 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-// /*
-//  * Code for convert from Gregorian date to hijri date
-//  */
-// function intPart($float)
-// {
-//     if ($float < -0.0000001)
-//         return ceil($float - 0.0000001);
-//     else
-//         return floor($float + 0.0000001);
-// }
-
-// function Greg2Hijri($day, $month, $year, $string = false)
-// {
-//     $day   = (int) $day;
-//     $month = (int) $month;
-//     $year  = (int) $year;
-
-//     if (($year > 1582) or (($year == 1582) and ($month > 10)) or (($year == 1582) and ($month == 10) and ($day > 14)))
-//     {
-//         $jd = intPart((1461*($year+4800+intPart(($month-14)/12)))/4)+intPart((367*($month-2-12*(intPart(($month-14)/12))))/12)-
-//         intPart( (3* (intPart(  ($year+4900+    intPart( ($month-14)/12)     )/100)    )   ) /4)+$day-32075;
-//     }
-//     else
-//     {
-//         $jd = 367*$year-intPart((7*($year+5001+intPart(($month-9)/7)))/4)+intPart((275*$month)/9)+$day+1729777;
-//     }
-
-//     $l = $jd-1948440+10632;
-//     $n = intPart(($l-1)/10631);
-//     $l = $l-10631*$n+354;
-//     $j = (intPart((10985-$l)/5316))*(intPart((50*$l)/17719))+(intPart($l/5670))*(intPart((43*$l)/15238));
-//     $l = $l-(intPart((30-$j)/15))*(intPart((17719*$j)/50))-(intPart($j/16))*(intPart((15238*$j)/43))+29;
-
-//     $month = intPart((24*$l)/709);
-//     $day   = $l-intPart((709*$month)/24);
-//     $year  = 30*$n+$j-30;
-
-//     $date = array();
-//     $date['year']  = $year;
-//     $date['month'] = $month;
-//     $date['day']   = $day;
-
-//     if (!$string)
-//         return $date;
-//     else
-//         return     "{$year}-{$month}-{$day}";
-// }
 
 // add_action('after_render_single_aside_menu', 'my_custom_menu_items');
 hooks()->add_action('admin_init', 'my_custom_setup_menu_items');
@@ -554,4 +507,13 @@ function get_legal_service_slug_by_id($service_id)
         return $service->slug;
     }
     return false;
+}
+
+hooks()->add_filter('sms_gateways', 'add_sms_gateway');
+
+function add_sms_gateway($gateways)
+{
+    array_push($gateways, 'sms/sms_mobily');
+    
+    return $gateways;
 }
--- application/models/Authentication_model.php
+++ application/models/Authentication_model.php
@@ -640,110 +640,13 @@ class Authentication_model extends App_Model
 	// ShababSy.com Added this func
 	public function send_verification_sms($userdata)
     {
-
-        $apiKet = "e0e3f79f5580ea54d9835234ced73f70";
-		$userAccount = 'mohd1978';
-        $passAccount = '19781978';
-        $sender 	 = 'TL4T';
-		
-		
-		
-        //الرسائل الناتجه من دالة الإرسال
-        $arraySendMsg = array();
-        $arraySendMsg[0] = "لم يتم الاتصال بالخادم";
-        $arraySendMsg[1] = "تمت عملية الإرسال بنجاح";
-        $arraySendMsg[2] = "رصيدك 0 , الرجاء إعادة التعبئة حتى تتمكن من إرسال الرسائل";
-        $arraySendMsg[3] = "رصيدك غير كافي لإتمام عملية الإرسال";
-        $arraySendMsg[4] = "رقم الجوال (إسم المستخدم) غير صحيح";
-        $arraySendMsg[5] = "كلمة المرور الخاصة بالحساب غير صحيحة";
-        $arraySendMsg[6] = "صفحة الانترنت غير فعالة , حاول الارسال من جديد";
-        $arraySendMsg[7] = "نظام غير فعال";
-        $arraySendMsg[8] = "تكرار رمز  لنفس المستخدم";
-        $arraySendMsg[9] = "انتهاء الفترة التجريبية";
-        $arraySendMsg[10] = "عدد الارقام لا يساوي عدد الرسائل";
-        $arraySendMsg[11] = "اشتراكك لا يتيح لك ارسال رسائل لهذه. يجب عليك تفعيل الاشتراك لهذه";
-        $arraySendMsg[12] = "إصدار البوابة غير صحيح";
-        $arraySendMsg[13] = "الرقم المرسل به غير مفعل أو لا يوجد الرمز BS في نهاية الرسالة";
-        $arraySendMsg[14] = "غير مصرح لك بالإرسال بإستخدام هذا المرسل";
-        $arraySendMsg[15] = "الأرقام المرسل لها غير موجوده أو غير صحيحه";
-        $arraySendMsg[16] = "إسم المرسل فارغ، أو غير صحيح";
-        $arraySendMsg[17] = "نص الرسالة غير متوفر أو غير مشفر بشكل صحيح";
-        $arraySendMsg[18] = "تم ايقاف الارسال من المزود";
-        $arraySendMsg[19] = "لم يتم العثور على مفتاح نوع التطبيق";
-
-        
         is_numeric($this->set_two_factor_auth_code($userdata->staffid,2)) ? $code= $this->set_two_factor_auth_code($userdata->staffid,2) : "Unavailable";
         $numbers = $userdata->phonenumber;
         $msg = 'Your verification code is: ' . $code;
-        $MsgID = time();//rand(1, 99999);
-        $timeSend = 0;
-        $dateSend = 0;
-        $deleteKey = 0;
-        $viewResult = 0;
 
-        $url = "www.mobily.ws/api/msgSend.php";
-        $applicationType = "68";
-        $sender = urlencode($sender);
-        $domainName = $_SERVER['SERVER_NAME'];
-        $stringToPost = "mobile=" . $userAccount . "&password=" . $passAccount . "&numbers=" . $numbers . "&sender=" . $sender . "&msg=" . $msg . "&timeSend=" . $timeSend . "&dateSend=" . $dateSend . "&applicationType=" . $applicationType . "&domainName=" . $domainName . "&msgId=" . $MsgID . "&deleteKey=" . $deleteKey . "&lang=3";
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $stringToPost);
-        $result = curl_exec($ch);
-
-        if ($viewResult)
-            $result = $this->printStringResult(trim($result), $arraySendMsg);
-        
+        // Send verification code with activated sms gateway
+        $result =$this->app_sms->g_send($numbers,$msg); 
        
         return $result;
-    }
-
-
-	// ShababSy.com Added this func
-    public function printStringResult($apiResult, $arrayMsgs, $printType = 'Alpha')
-    {
-        global $undefinedResult;
-        switch ($printType) {
-            case 'Alpha': {
-                if (array_key_exists($apiResult, $arrayMsgs))
-                    return $arrayMsgs[$apiResult];
-                else
-                    return $arrayMsgs[0];
-            }
-                break;
-
-            case 'Balance': {
-                if (array_key_exists($apiResult, $arrayMsgs))
-                    return $arrayMsgs[$apiResult];
-                else {
-                    list($originalAccount, $currentAccount) = explode("/", $apiResult);
-                    if (!empty($originalAccount) && !empty($currentAccount)) {
-                        return sprintf($arrayMsgs[3], $currentAccount, $originalAccount);
-                    } else
-                        return $arrayMsgs[0];
-                }
-            }
-                break;
-
-            case 'Senders': {
-                $apiResult = str_replace('[pending]', '[pending]<br>', $apiResult);
-                $apiResult = str_replace('[active]', '<br>[active]<br>', $apiResult);
-                $apiResult = str_replace('[notActive]', '<br>[notActive]<br>', $apiResult);
-                return $apiResult;
-            }
-                break;
-
-            case 'Normal':
-                if ($apiResult{0} != '#')
-                    return $arrayMsgs[$apiResult];
-                else
-                    return $apiResult;
-                break;
-        }
     }
 }

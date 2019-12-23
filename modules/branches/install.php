8dc9d322d198410798881de14bd2b3cad217c6a1
zaher <ahmad.khrezaty@gmail.com>
2019-12-23 08:54:04 -0500

    Branch Add Branch Services To Clients And Opponents And Staff And Departments

--- modules/branches/install.php
+++ modules/branches/install.php
@@ -28,7 +28,22 @@ if (!$CI->db->table_exists(db_prefix() . 'branches')) {
     `zip_code` varchar(255) NOT NULL,
     `username` varchar(255) NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=' . $CI->db->char_set . ';');
+
+  $data = [
+    'title_en' => 'General branch', 
+    'title_ar' => 'الفرع العام', 
+    'city_id' => '338', 
+    'country_id' => '217', 
+    'registraion_number' => '1'
+  ];
+  $CI->db->insert('tblbranches', $data);
+  $insert_id = $CI->db->insert_id();
+  if ($insert_id) {
+      log_activity('New Branches Added [' . $data['title_en'] . ']');
+      return $insert_id;
+  }
 }
+
 
 if (!$CI->db->table_exists(db_prefix() . 'branches_services')) {
     $CI->db->query('CREATE TABLE `' . db_prefix() . 'branches_services` (                   
@@ -37,6 +52,67 @@ if (!$CI->db->table_exists(db_prefix() . 'branches_services')) {
      `rel_type` varchar(25) NOT NULL,
      `rel_id` int(11) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=' . $CI->db->char_set . ';');
+
+
+    $staffs = $CI->db->get(db_prefix() . 'staff')->result_array();
+    foreach ($staffs as $staff){
+      $data = [
+        'branch_id' => 1, 
+        'rel_type' => 'staff', 
+        'rel_id' => $staff['staffid']
+      ];
+      $CI->db->insert('tblbranches_services', $data);
+      $insert_id = $CI->db->insert_id();
+
+      if ($insert_id) {
+          log_activity('Add Branch ['. $data['branch_id'] .'] To '.$data['rel_type'].' [' . $data['rel_id'] . ']');
+      }
+    }
+
+    $departments = $CI->db->get(db_prefix() . 'departments')->result_array();
+    foreach ($departments as $department){
+      $data = [
+        'branch_id' => 1, 
+        'rel_type' => 'departments', 
+        'rel_id' => $department['departmentid']
+      ];
+      $CI->db->insert('tblbranches_services', $data);
+      $insert_id = $CI->db->insert_id();
+
+      if ($insert_id) {
+          log_activity('Add Branch ['. $data['branch_id'] .'] To '.$data['rel_type'].' [' . $data['rel_id'] . ']');
+      }
+    }
+
+    $clients = $CI->db->get(db_prefix() . 'clients')->result_array();
+    foreach ($clients as $client){
+      if($client['client_type'] == 0){
+        $data = [
+          'branch_id' => 1, 
+          'rel_type' => 'clients', 
+          'rel_id' => $client['userid']
+        ];
+        $CI->db->insert('tblbranches_services', $data);
+        $insert_id = $CI->db->insert_id();
+
+        if ($insert_id) {
+            log_activity('Add Branch ['. $data['branch_id'] .'] To '.$data['rel_type'].' [' . $data['rel_id'] . ']');
+        }
+      }elseif($client['client_type'] == 1){
+        $data = [
+          'branch_id' => 1, 
+          'rel_type' => 'opponent', 
+          'rel_id' => $client['userid']
+        ];
+        $CI->db->insert('tblbranches_services', $data);
+        $insert_id = $CI->db->insert_id();
+
+        if ($insert_id) {
+            log_activity('Add Branch ['. $data['branch_id'] .'] To '.$data['rel_type'].' [' . $data['rel_id'] . ']');
+        }
+      }
+    }
+    
 }
 // Add branch_id column to department table
 if (!$CI->db->field_exists('branch_id', db_prefix().'departments')){

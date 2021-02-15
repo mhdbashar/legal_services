e33de37378b7ffd94b34ae7d66f61d5f7a14d986
DESKTOP-U04FTGO\User <ahmad.khrezaty@gmail.com>
2021-02-15 03:53:02 +0200

    Remove branch from organization ( designations, sub_departments )

--- modules/hr/views/organization/modals/sub_department_modal.php
+++ modules/hr/views/organization/modals/sub_department_modal.php
@@ -16,28 +16,27 @@
                         <?php echo render_input('sub_department_name','sub_department_name', '', 'text', ['required' => 'required']); ?>
                     </div>
                 </div>
-            <?php  if($this->app_modules->is_active('branches')){  ?>
-                <div class="form-group">
-                    <label for="branch_id" class="control-label"><?php echo _l('branch') ?></label>
-                    <select class="form-control" id="a_branch_id" name="branch_id" placeholder="<?php echo _l('branch') ?>" aria-invalid="false">
-                        <option></option>
-                    <?php foreach ($branches as $value) { ?>
-                        <option value="<?php echo $value['key'] ?>"><?php echo $value['value'] ?></option>
-                    <?php } ?>
-                    </select>     
-                </div>
-            <?php } ?>
+<!--            --><?php // if($this->app_modules->is_active('branches')){  ?>
+<!--                <div class="form-group">-->
+<!--                    <label for="branch_id" class="control-label">--><?php //echo _l('branch') ?><!--</label>-->
+<!--                    <select class="form-control" id="a_branch_id" name="branch_id" placeholder="--><?php //echo _l('branch') ?><!--" aria-invalid="false">-->
+<!--                        <option></option>-->
+<!--                    --><?php //foreach ($branches as $value) { ?>
+<!--                        <option value="--><?php //echo $value['key'] ?><!--">--><?php //echo $value['value'] ?><!--</option>-->
+<!--                    --><?php //} ?>
+<!--                    </select>     -->
+<!--                </div>-->
+<!--            --><?php //} ?>
                 <div class="form-group">
                     <label for="department_id" class="control-label"><?php echo _l('department') ?></label>
                     <select class="form-control" id="a_department_id" name="department_id" placeholder="<?php echo _l('department') ?>" aria-invalid="false">
                         <option></option>
                         <?php
-                        if(!$this->app_modules->is_active('branches')){
                          foreach ($departments as $department) { ?>
                             <option value="<?php echo $department['departmentid'] ?>">
                                 <?php echo $department['name'] ?>
                             </option>
-                        <?php }} ?>
+                        <?php } ?>
                     </select>     
                 </div>
             </div>
@@ -66,28 +65,16 @@
                         <?php echo render_input('sub_department_name','sub_department_name', '', 'text', ['required' => 'required']); ?>
                     </div>
                 </div>
-            <?php  if($this->app_modules->is_active('branches')){  ?>
-                <div class="form-group">
-                    <label for="branch_id" class="control-label"><?php echo _l('branch') ?></label>
-                    <select class="form-control" id="branch_id" name="branch_id" placeholder="<?php echo _l('branch') ?>" aria-invalid="false">
-                        <option></option>
-                    <?php foreach ($branches as $value) { ?>
-                        <option class="department_id" value="<?php echo $value['key'] ?>"><?php echo $value['value'] ?></option>
-                    <?php } ?>
-                    </select>     
-                </div>
-            <?php } ?>
                 <div class="form-group">
                     <label for="department_id" class="control-label"><?php echo _l('department') ?></label>
                     <select class="form-control" id="department_id" name="department_id" placeholder="<?php echo _l('department') ?>" aria-invalid="false">
                         <option></option>
                         <?php
-                        if(!$this->app_modules->is_active('branches')){
                          foreach ($departments as $department) { ?>
                             <option value="<?php echo $department['departmentid'] ?>">
                                 <?php echo $department['name'] ?>
                             </option>
-                        <?php }} ?>
+                        <?php } ?>
                     </select>     
                 </div>
             </div>
@@ -120,36 +107,9 @@
                 $('[name="id"]').val(data.id);
 
                 $('[name="sub_department_name"]').val(data.sub_department_name);
-            <?php  if($this->app_modules->is_active('branches')){  ?>    
-                $.get(admin_url + 'hr/organization/get_departments_by_branch_id/' + data.branch.branch_id, function(response) {
-                    if (response.success == true) {
-                        $('#a_department_id').empty();
-                        $('#a_department_id').append($('<option>', {
-                            value: '',
-                            text: ''
-                        }));
-                        for(let i = 0; i < response.data.length; i++) {
-                            let key = response.data[i].key;
-                            let value = response.data[i].value;
-                            let select = false;
-                            if(data.department.departmentid == key)
-                                select = true;
-                            $('#a_department_id').append($('<option>', {
-                                value: key,
-                                text: value,
-                                selected: select
-                            }));
-                            $('#a_department_id').selectpicker('refresh');
-                        }
-                    } else {
-                        alert_float('danger', response.message);
-                    }
-                }, 'json');
-                $('[name="branch_id"]').val(data.branch.branch_id);
 
-            <?php  }  ?>
-                $('[name="department_id"]').val(data.department.departmentid);
-                $('#a_department_id').selectpicker('refresh');
+                $('[name="department_id"]').val(data.department_id);
+                // $('#a_department_id').selectpicker('refresh');
 
 
                 $('#update_sub_department').modal('show'); // show bootstrap modal when complete loaded

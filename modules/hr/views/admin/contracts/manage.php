92a992e7951e2ff1f01b41d1eb4913f91d7a585b
DESKTOP-U04FTGO\User <ahmad.khrezaty@gmail.com>
2021-03-22 01:49:04 +0200

    Remove contracts header from hr contracts table

--- modules/hr/views/admin/contracts/manage.php
+++ modules/hr/views/admin/contracts/manage.php
@@ -9,54 +9,6 @@
                         <?php if(has_permission('contracts','','create')){ ?>
                             <a href="<?php echo admin_url('hr/contracts/contract'); ?>" class="btn btn-info pull-left display-block"><?php echo _l('new_contract'); ?></a>
                         <?php } ?>
-                        <?php $this->load->view('admin/contracts/filters'); ?>
-                        <div class="clearfix"></div>
-                        <hr class="hr-panel-heading" />
-                        <div class="row" id="contract_summary">
-                            <div class="col-md-12">
-                                <h4 class="no-margin text-success"><?php echo _l('contract_summary_heading'); ?></h4>
-                            </div>
-                            <div class="col-md-2 col-xs-6 border-right">
-                                <h3 class="bold"><?php echo $count_active; ?></h3>
-                                <span class="text-info"><?php echo _l('contract_summary_active'); ?></span>
-                            </div>
-                            <div class="col-md-2 col-xs-6 border-right">
-                                <h3 class="bold"><?php echo $count_expired; ?></h3>
-                                <span class="text-danger"><?php echo _l('contract_summary_expired'); ?></span>
-                            </div>
-                            <div class="col-md-2 col-xs-6 border-right">
-                                <h3 class="bold"><?php echo count($expiring); ?></h3>
-                                <span class="text-warning"><?php echo _l('contract_summary_about_to_expire'); ?></span>
-                            </div>
-                            <div class="col-md-2 col-xs-6 border-right">
-                                <h3 class="bold"><?php echo $count_recently_created; ?></h3>
-                                <span class="text-success"><?php echo _l('contract_summary_recently_added'); ?></span>
-                            </div>
-                            <div class="col-md-2 col-xs-6">
-                                <h3 class="bold"><?php echo $count_trash; ?></h3>
-                                <span class="text-muted"><?php echo _l('contract_summary_trash'); ?></span>
-                            </div>
-                            <div class="clearfix"></div>
-                            <hr class="hr-panel-heading" />
-                            <div class="col-md-6 border-right">
-                                <h4><?php echo _l('contract_summary_by_type'); ?></h4>
-                                <div class="relative" style="max-height:400px">
-                                    <canvas class="chart" height="400" id="contracts-by-type-chart"></canvas>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <h4>
-                                    <?php echo _l('contract_summary_by_type_value'); ?>
-                                    (<span data-toggle="tooltip"
-                                           data-title="<?php echo _l('base_currency_string'); ?>"
-                                           class="text-has-action">
-                                        <?php echo $base_currency->name; ?></span>)
-                                </h4>
-                                <div class="relative" style="max-height:400px">
-                                    <canvas class="chart" height="400" id="contracts-value-by-type-chart"></canvas>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div class="panel_s">

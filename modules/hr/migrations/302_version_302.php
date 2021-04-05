607d1aade07ac1448b86ce629ac410d69e79d5d2
DESKTOP-U04FTGO\User <ahmad.khrezaty@gmail.com>
2021-04-05 03:24:56 +0300

    Active contract code in HRM contracts

--- modules/hr/migrations/302_version_302.php
+++ modules/hr/migrations/302_version_302.php
@@ -12,22 +12,26 @@ class Migration_Version_302 extends App_module_migration
 
         add_option('next_hr_contract_number', '1');
 
+        add_option('hr_contract_number', '1');
+
         add_option('hr_staff_prefix', '#STAFF');
 
         add_option('next_hr_staff_number', '1');
 
-        if (!$CI->db->field_exists('code', db_prefix() . 'hr_contracts')) {
-            $CI->db->query("ALTER TABLE `".db_prefix() ."hr_contracts` ADD `code` varchar(255) DEFAULT '';");
-        }
-        if (!$CI->db->field_exists('code_number', db_prefix() . 'hr_contracts')) {
-            $CI->db->query("ALTER TABLE `".db_prefix() ."hr_contracts` ADD `code_number` varchar(255) DEFAULT '';");
-        }
+        add_option('hr_staff_number', '1');
 
-        if (!$CI->db->field_exists('code', db_prefix() . 'hr_extra_info')) {
-            $CI->db->query("ALTER TABLE `".db_prefix() ."hr_extra_info` ADD `code` varchar(255) DEFAULT '';");
+        if (!$CI->db->field_exists('number', db_prefix() . 'hr_contracts')) {
+            $CI->db->query("ALTER TABLE `".db_prefix() ."hr_contracts` ADD `number` varchar(255) DEFAULT '';");
         }
-        if (!$CI->db->field_exists('code_number', db_prefix() . 'hr_extra_info')) {
-            $CI->db->query("ALTER TABLE `".db_prefix() ."hr_extra_info` ADD `code_number` varchar(255) DEFAULT '';");
+//
+//        if (!$CI->db->field_exists('number_format', db_prefix() . 'hr_extra_info')) {
+//            $CI->db->query("ALTER TABLE `".db_prefix() ."hr_extra_info` ADD `number_format` varchar(255) DEFAULT '1';");
+//        }
+//        if (!$CI->db->field_exists('prefix', db_prefix() . 'hr_extra_info')) {
+//            $CI->db->query("ALTER TABLE `".db_prefix() ."hr_extra_info` ADD `prefix` varchar(255) DEFAULT '';");
+//        }
+        if (!$CI->db->field_exists('number', db_prefix() . 'hr_extra_info')) {
+            $CI->db->query("ALTER TABLE `".db_prefix() ."hr_extra_info` ADD `number` varchar(255) DEFAULT '';");
         }
 
     }

a0c6600d00791b119c45644ff10e7102a32dd6a2
DESKTOP-U04FTGO\User <ahmad.khrezaty@gmail.com>
2021-04-14 23:13:32 +0300

    Install resource_workload module

---
+++ modules/resource_workload/language/arabic/resource_workload_lang.php
@@ -0,0 +1,33 @@
+<?php
+
+$lang['resource_workload'] = 'Staff workload';
+$lang['note_workload'] = 'E: Estimated Hours, S: Spent hours';
+$lang['workload'] = 'Workload';
+$lang['timeline'] = 'Timeline';
+$lang['chart'] = 'Chart';
+$lang['estimate_overload'] = 'Estimated hours overload';
+$lang['estimate_normal'] = 'Estimated hours normal';
+$lang['spent_normal'] = 'Spent hours normal';
+$lang['spent_overload'] = 'Spent hours overload';
+$lang['number_of_tasks'] = 'Number of tasks';
+$lang['statistics_by_estimate_hours'] = 'Statistics by estimate hours';
+$lang['statistics_by_spent_hours'] = 'Statistics by spent hours';
+$lang['statistics_by_departments'] = 'Statistics by departments';
+$lang['overload'] = 'Overload';
+$lang['normal'] = 'Normal';
+$lang['standard_workload'] = 'Standard workload per day (hours)';
+$lang['total_standard_hours'] = 'Total standard hours';
+$lang['total_overload_hours'] = 'Total overload hours';
+$lang['total_hours'] = 'Total hours';
+$lang['not_in_department'] = 'Not in Department';
+$lang['working_days_of_the_week'] = 'Working days of the week';
+$lang['kanban'] = 'Kanban';
+$lang['reason_holidays'] = 'Name';
+$lang['date_off'] = 'Holidays';
+$lang['new_leave'] = 'New holidays';
+$lang['manage_dayoff'] = 'Holidays';
+$lang['general_settings'] = 'General settings';
+$lang['edit_leave'] = 'Edit holidays';
+$lang['creator'] = 'Creator';
+$lang['customize_standard_workload'] = 'Staff Shifting';
+

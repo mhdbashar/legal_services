6b4a9c3ef0db27114f26e270a0271732fd269d55
DESKTOP-U04FTGO\User <ahmad.khrezaty@gmail.com>
2021-04-23 00:49:28 +0300

    Fix direction in HRM organization charts

--- modules/hr/views/organizational/organizational_chart.php
+++ modules/hr/views/organizational/organizational_chart.php
@@ -132,7 +132,7 @@
             <div class="modal-body">
                 <div class="row">
                     <div class="col-md-12" id="dp_chart">
-                        <div id="department_chart"></div>
+                        <div id="department_chart" style="direction: ltr"></div>
                     </div>
                 </div>
             </div>                 

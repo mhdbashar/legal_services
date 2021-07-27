fc60dd3585a68e3dbeee3c7a89ae505ccfba5a35
Baraa Alhalabi <baraa-alhalabi@hotmail.com>
2021-07-27 22:32:15 +0300

    Add new module receive SMS

---
+++ modules/receive_sms/language/english/receive_sms_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['device_sms'] = 'Device SMS';
+
+$lang['receive_sms'] = 'Receive SMS';
+
+$lang['receive_sms_token'] = 'Receive SMS Token';
+
+$lang['receive_sms_device'] = 'Receive SMS Device';
+
+$lang['messages'] = 'Messages';
+
+$lang['message'] = 'Message';
+
+$lang['sender'] = 'Sender';
+
+$lang['error'] = 'Error!';
+
+$lang['sms_senders'] = 'SMS Senders';
+
+$lang['saved_messages'] = 'Saved Messages';
+
+$lang['created_at'] = 'Created at';

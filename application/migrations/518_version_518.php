893a971660d8df9e20ae29a2b409d347bb36dc1d
Ahmad Zaher Khrezaty <ahmad.khrezaty@gmail.com>
2022-03-09 19:27:34 +0200

    Add invoice_info_format to organization settings

--- application/migrations/518_version_518.php
+++ application/migrations/518_version_518.php
@@ -15,5 +15,15 @@ class Migration_Version_518 extends CI_Migration
         $automatically_send_lawyer_daily_agenda = date('H:i', strtotime($automatically_send_lawyer_daily_agenda . 'PM'));
         update_option('automatically_send_lawyer_daily_agenda', $automatically_send_lawyer_daily_agenda);
         add_option('daily_agenda_last_check', date('Y-m-d'));
+        add_option('invoice_info_format', '{company_name} اسم الشركة : <br />
+{building_number} رقم المبنى : <br />
+{street_name} اسم الشارع:<br />
+{district_name}  الحي: <br />
+{city}  المدينة: <br />
+{country} البلد: <br />
+{zip_code} الرمز البريدي:<br />
+{additional_number} الرقم الاضافي للعنوان: <br />
+{vat_number} رقم تسجيل ضريبة القيمة المضافة:<br />
+{other_number} معرف اّخر:');
     }
 }

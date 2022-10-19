04564cb9da610e20c576f11b6207a09c0acc4434
Hiba Kharma <hibakharma@gmail.com>
2022-10-19 10:35:13 +0400

    test11

--- application/controllers/admin/legalservices/Regular_durations.php
+++ application/controllers/admin/legalservices/Regular_durations.php
@@ -33,7 +33,7 @@ class Regular_durations extends AdminController
                 set_alert('success', _l('added_successfully', _l('regular_duration')));
                // send_mail_template('regular_duration_deadline_notification', "hibakharma@gmail.com", 2,  1);
 
-                send_mail_template('staff_created', "hibakharma@gmail.com", 2, "123");
+               // send_mail_template('staff_created', "hibakharma@gmail.com", 2, "123");
                 redirect(admin_url("legalservices/regular_durations"));
 
             }

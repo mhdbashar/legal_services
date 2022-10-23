3ecd86bee5289e55077a25d2c001de044e2c477d
mohammed nour <mohammednour944@gmail.com>
2022-10-23 06:56:34 +0300

    noor - add note to help library

--- application/migrations/520_version_520.php
+++ application/migrations/520_version_520.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Migration_Version_520 extends CI_Migration
 {
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
     }
@@ -12,8 +12,7 @@ class Migration_Version_520 extends CI_Migration
     public function up()
     {
         if (!$this->db->field_exists('add_to_library', db_prefix() . 'staff')) {
-            $this->db->query("ALTER TABLE `tblstaff` ADD `add_to_library` tinyint(1) NOT NULL DEFAULT '0';");
+            $this->db->query("ALTER TABLE `tblstaff` ADD `add_to_library` tinyint(1) NOT NULL DEFAULT '0'");
         }
-
     }
 }

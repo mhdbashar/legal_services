ae9b8455fb46f3ba464a281fe5c9411fd8aae67a
mohammed nour <mohammednour944@gmail.com>
2022-12-25 05:44:04 +0200

    fix mail template next_session_action

--- application/libraries/mails/Send_report_session_to_customer.php
+++ application/libraries/mails/Send_report_session_to_customer.php
@@ -10,25 +10,24 @@ class Send_report_session_to_customer extends App_mail_template
 
     protected $contact;
 
-    protected $client_id;
-
     public $slug = 'send_report_session';
 
     public $rel_type = 'sessions';
 
-    public function __construct($client_id,$contact,$session, $cc = '')
+    public function __construct($contact,$session, $cc = '')
     {
         parent::__construct();
 
         $this->session      = $session;
         $this->contact      = $contact;
-        $this->client_id    = $client_id;
         $this->cc           = $cc;
     }
 
     public function build()
     {
-        $this->to($this->contact->email)->set_merge_fields('sessions_merge_fields',$this->session->id)
-            ->set_merge_fields('client_merge_fields',$this->client_id, $this->contact->id);
+        $this->to($this->contact->email)
+            ->set_rel_id($this->task_id)
+            ->set_merge_fields('sessions_merge_fields',$this->session->id)
+            ->set_merge_fields('client_merge_fields',$this->contact->userid, $this->contact->id);
     }
 }

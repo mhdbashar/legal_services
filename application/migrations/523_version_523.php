3889ade2155902150346ca93439f99e350045209
mohammed nour <mohammednour944@gmail.com>
2023-01-22 05:17:56 +0200

    add contact_notification to sessions

--- application/migrations/523_version_523.php
+++ application/migrations/523_version_523.php
@@ -15,10 +15,10 @@ class Migration_Version_523 extends CI_Migration
             $this->db->query('ALTER TABLE `' . db_prefix() . 'my_session_info` ADD `clientid` int(11) NULL DEFAULT NULL');
         }
         if (!$this->db->field_exists('contact_notification', db_prefix() . 'my_session_info')) {
-            $this->db->query('ALTER TABLE `' . db_prefix() . 'my_cases` ADD `contact_notification` INT DEFAULT 1');
+            $this->db->query('ALTER TABLE `' . db_prefix() . 'my_session_info` ADD `contact_notification` INT DEFAULT 1');
         }
         if (!$this->db->field_exists('notify_contacts', db_prefix() . 'my_session_info')) {
-            $this->db->query('ALTER TABLE `' . db_prefix() . 'my_cases` ADD `notify_contacts` TEXT DEFAULT NULL');
+            $this->db->query('ALTER TABLE `' . db_prefix() . 'my_session_info` ADD `notify_contacts` TEXT DEFAULT NULL');
         }
     }
 }

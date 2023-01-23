12b57946a74d4ef603ce13b4bdde04ebc86ee155
mohammed nour <mohammednour944@gmail.com>
2023-01-23 04:54:10 +0200

    remove knowledg helper to autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -163,7 +163,6 @@ $autoload['helper'] = [
         'oservices',
         'iservices',
         'telegram',
-        'my_knowledge_base_helper',
     ];
 
 if (file_exists(APPPATH . 'helpers/my_functions_helper.php')) {

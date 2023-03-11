5cb9d12e5a576f84cfe932ba9527d043ac1b995d
ryadshaker <ryadshaker461@gmail.com>
2023-03-11 13:11:16 +0200

    Alter footer and header

--- application/libraries/pdf/Payment_pdf.php
+++ application/libraries/pdf/Payment_pdf.php
@@ -54,4 +54,48 @@ class Payment_pdf extends App_pdf
 
         return $actualPath;
     }
+    public function Header()
+    {
+
+        $image_file = get_option('custom_pdf_header_image_url');
+        $width = get_option('pdf_header_logo_width');
+
+        if ($image_file != '') {
+
+            $this->SetMargins(10, 60, 10, true);
+            if ($width != '') {
+
+                $this->Image($image_file, 0, 0, $width, 0, 'PNG', '', 'T', false, 300, 'C', false, false, 0, false, false, false);
+            } else {
+                $width = 210;
+                $this->Image($image_file, 0, 0, $width, 50, 'PNG', '', 'T', false, 300, 'C', false, false, 0, false, false, false);
+            }
+
+        }
+    }
+
+    public function Footer()
+    {
+        $width_footer = '';
+        $image_file_footer = '';
+        $image_file_footer = get_option('custom_pdf_footer_image_url');
+
+        $width_footer = get_option('pdf_footer_logo_width');
+
+        if ($image_file_footer != '') {
+            $this->SetMargins(10, 60, 10, true);
+            $this->SetAutoPageBreak(true, 60);
+            if ($width_footer != '') {
+
+                $this->Image($image_file_footer, 0, 280, $width_footer, 0, 'PNG', '', 'B', false, 300, 'C', false, false, 0, false, false, false);
+
+            } else {
+                $width_footer = 210;
+
+                $this->Image($image_file_footer, 0, 265, $width_footer, 0, 'PNG', '', 'B', false, 300, 'C', false, false, 0, false, false, false);
+
+            }
+
+        }
+    }
 }

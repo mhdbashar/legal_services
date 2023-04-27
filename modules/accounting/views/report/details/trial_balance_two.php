26986e9962a2e64a6a5c6f986df34075c089d41e
Ahmad Zaher Khrezaty <ahmad.khrezaty@gmail.com>
2023-04-27 03:16:49 +0300

    Fix total in trail balance report

--- modules/accounting/views/report/details/trial_balance_two.php
+++ modules/accounting/views/report/details/trial_balance_two.php
@@ -61,15 +61,29 @@
         <?php
           $row_index = 0;
           $parent_index = 0;
-          $total_credit = 0;
-          $total_debit = 0;
+        $total_credit = 0;
+        $total_debit = 0;
+
+
+        $total_opening_stock_debit = 0;
+        $total_opening_stock_credit = 0;
+
+
+        $total_ending_stock_debit = 0;
+        $total_ending_stock_credit = 0;
 //          echo '<pre>'; print_r($data_report); exit;
 
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['cash_and_cash_equivalents'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
           echo html_entity_decode($data['html']);
-          $total_debit += $data['total_debit'];
-          $total_credit += $data['total_credit'];
+        $total_debit += $data['total_debit'];
+        $total_credit += $data['total_credit'];
+
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
 
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['accounts_receivable'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
@@ -77,11 +91,23 @@
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
 
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
+
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['current_assets'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
           echo html_entity_decode($data['html']);
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
+
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
 
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['fixed_assets'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
@@ -89,11 +115,23 @@
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
 
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
+
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['non_current_assets'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
           echo html_entity_decode($data['html']);
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
+
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
 
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['accounts_payable'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
@@ -101,11 +139,23 @@
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
 
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
+
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['credit_card'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
           echo html_entity_decode($data['html']);
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
+
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
 
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['current_liabilities'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
@@ -113,11 +163,23 @@
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
 
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
+
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['non_current_liabilities'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
           echo html_entity_decode($data['html']);
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
+
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
 
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['owner_equity'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
@@ -125,11 +187,23 @@
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
 
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
+
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['income'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
           echo html_entity_decode($data['html']);
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
+
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
 
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['cost_of_sales'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
@@ -137,11 +211,23 @@
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
 
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
+
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['expenses'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
           echo html_entity_decode($data['html']);
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
+
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
 
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['other_income'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
@@ -149,11 +235,23 @@
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
 
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
+
           $data = $this->accounting_model->get_html_trial_balance_two($data_report['data']['other_expenses'], ['html' => '', 'row_index' => $row_index + 1, 'total_credit' => 0, 'total_debit' => 0], $parent_index, $currency);
           $row_index = $data['row_index'];
           echo html_entity_decode($data['html']);
           $total_debit += $data['total_debit'];
           $total_credit += $data['total_credit'];
+
+        $total_opening_stock_debit += $data['total_opening_stock_debit'];
+        $total_opening_stock_credit += $data['total_opening_stock_credit'];
+
+        $total_ending_stock_debit += $data['total_ending_stock_debit'];
+        $total_ending_stock_credit += $data['total_ending_stock_credit'];
 
           ?>
      
@@ -164,8 +262,12 @@
                    </span>
 
             </td>
-            <td class="total_amount"><?php echo app_format_money($total_debit, $currency->name); ?> </td>
-            <td class="total_amount"><?php echo app_format_money($total_credit, $currency->name); ?> </td>
+              <td class="total_amount"><?php echo app_format_money($total_opening_stock_debit, $currency->name); ?> </td>
+              <td class="total_amount"><?php echo app_format_money($total_opening_stock_credit, $currency->name); ?> </td>
+              <td class="total_amount"><?php echo app_format_money($total_debit, $currency->name); ?> </td>
+              <td class="total_amount"><?php echo app_format_money($total_credit, $currency->name); ?> </td>
+              <td class="total_amount"><?php echo app_format_money($total_ending_stock_debit, $currency->name); ?> </td>
+              <td class="total_amount"><?php echo app_format_money($total_ending_stock_credit, $currency->name); ?> </td>
           </tr>
         </tbody>
     </table>

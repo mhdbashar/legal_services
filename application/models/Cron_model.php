c616ce4e92f966ef13a5c855695adf375b2c1282
Hiba Kharma <hibakharma@gmail.com>
2023-06-10 22:10:12 +0400

    ..

--- application/models/Cron_model.php
+++ application/models/Cron_model.php
@@ -1104,12 +1104,13 @@ class Cron_model extends App_Model
                 $start_date         = strtotime($case['start_date']);
                 $start_and_end_date_diff = $end_date - $start_date;
                 $start_and_end_date_diff = floor($start_and_end_date_diff / (60 * 60 * 24));
-                // if ($diff <= $reminder_before && $start_and_end_date_diff < $reminder_before) {
-                     if ($diff <= $reminder_before ) {
+                 if ($diff <= $reminder_before && $start_and_end_date_diff < $reminder_before)
+              //  if ($diff <= $reminder_before )
+                     {
 
                          $this->db->where('project_id', $case['case_id']);
-                $assignees = $this->db->get(db_prefix() . 'my_members_cases')->result();
-                foreach ($assignees as $member) {
+                       $assignees = $this->db->get(db_prefix() . 'my_members_cases')->result();
+                       foreach ($assignees as $member) {
                     $this->db->where('staffid',$member->staff_id);
                     $row = $this->db->get(db_prefix() . 'staff')->row();
                     if ($row) {
@@ -1149,8 +1150,8 @@ class Cron_model extends App_Model
                             'deadline_notified' => 1,'regular_header' => 1,
                         ]);
                     }
-                }
                   }
+                     }
             }
         }
 

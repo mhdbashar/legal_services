82eccd9c170509df576e003b85b21c4e194e4ba8
Hiba Kharma <hibakharma@gmail.com>
2023-07-29 19:17:58 +0400

    add new migration529

---
+++ application/migrations/529_version_529.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Version_529 extends CI_Migration
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function up()
+    {
+
+        if (!$this->db->field_exists('proc_header', db_prefix() . 'procuration_cases')) {
+            $this->db->query('ALTER TABLE `' . db_prefix() . 'procuration_cases` ADD `proc_header` int(1) NULL DEFAULT 0');
+        }
+        if (!$this->db->field_exists('proc_alert_close', db_prefix() . 'procuration_cases')) {
+            $this->db->query('ALTER TABLE `' . db_prefix() . 'procuration_cases` ADD `proc_alert_close` date NULL DEFAULT NULL');
+        }
+
+
+
+    }
+    }

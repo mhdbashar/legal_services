385e798286dee8cfe4379d0d821e173d897fd539
Hiba Kharma <hibakharma@gmail.com>
2023-09-02 20:32:14 +0400

    fixing bug:during  create credit note  for new bill

--- modules/accounting/models/Accounting_model.php
+++ modules/accounting/models/Accounting_model.php
@@ -20399,7 +20399,7 @@ class Accounting_model extends App_Model
      * @return boolean
      */
     public function automatic_credit_note_conversion($data){
-        $this->delete_convert($data['credit_id'], 'credit_note');
+        $this->delete_convert($data['credit_note_id'], 'credit_note');
 
         $payment_account = get_option('acc_credit_note_payment_account');
         $deposit_to = get_option('acc_credit_note_deposit_to');
@@ -20431,7 +20431,7 @@ class Accounting_model extends App_Model
             $node['credit'] = 0;
             $node['date'] = date('Y-m-d');
             $node['description'] = '';
-            $node['rel_id'] = $data['credit_id'];
+            $node['rel_id'] = $data['credit_note_id'];
             $node['rel_type'] = 'credit_note';
             $node['datecreated'] = date('Y-m-d H:i:s');
             $node['addedfrom'] = get_staff_user_id();
@@ -20445,7 +20445,7 @@ class Accounting_model extends App_Model
             $node['debit'] = 0;
             $node['credit'] = $payment_total;
             $node['description'] = '';
-            $node['rel_id'] = $data['credit_id'];
+            $node['rel_id'] = $data['credit_note_id'];
             $node['rel_type'] = 'credit_note';
             $node['datecreated'] = date('Y-m-d H:i:s');
             $node['addedfrom'] = get_staff_user_id();

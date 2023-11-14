bfdf4fe55a2e8ad3fa747cd383c75457dc7c9d5f
Hiba Kharma <hibakharma@gmail.com>
2023-11-14 18:31:44 +0400

    fixing migration 532

--- application/migrations/535_version_535.php
+++ application/migrations/535_version_535.php
@@ -15,35 +15,35 @@ class Migration_Version_535 extends CI_Migration
 //*****ADDing tbltransaction_type table ***********//
 
         $this->db->query("CREATE TABLE IF NOT EXISTS `tbltransaction_type` (
-            `id` int(11) NOT NULL ,
+            `id` int(11) NOT NULL AUTO_INCREMENT,
             `name` varchar(50)   NOT NULL,
              PRIMARY KEY (`id`)
           )  ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");
 
 //*****ADDing 	tbltransaction_origin table ***********//
         $this->db->query("CREATE TABLE IF NOT EXISTS `tbltransaction_origin` (
-            `id` int(11) NOT NULL ,
+            `id` int(11) NOT NULL AUTO_INCREMENT,
             `name` varchar(50)   NOT NULL,
              PRIMARY KEY (`id`)
           )  ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");
 
 //*****ADDing tbltransaction_incoming_type table ***********//
         $this->db->query("CREATE TABLE IF NOT EXISTS `tbltransaction_incoming_type` (
-            `id` int(11) NOT NULL ,
+            `id` int(11) NOT NULL AUTO_INCREMENT,
             `name` varchar(50)   NOT NULL,
              PRIMARY KEY (`id`)
           )  ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");
 
 //*****ADDing tbltransaction_importance table ***********//
         $this->db->query("CREATE TABLE IF NOT EXISTS `tbltransaction_importance` (
-            `id` int(11) NOT NULL ,
+            `id` int(11) NOT NULL AUTO_INCREMENT,
             `name` varchar(50)   NOT NULL,
              PRIMARY KEY (`id`)
           )  ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");
 
 //*****ADDing tbltransaction_classification table ***********//
         $this->db->query("CREATE TABLE IF NOT EXISTS `tbltransaction_classification` (
-            `id` int(11) NOT NULL ,
+            `id` int(11) NOT NULL AUTO_INCREMENT,
             `name` varchar(50)   NOT NULL,
              PRIMARY KEY (`id`)
           )  ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");

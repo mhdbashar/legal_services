ca96e4bfea8d426d6217ab12082f74a548cea521
mohammad nour <mohammadnourwatfa@gmail.com>
2024-01-11 07:30:10 +0200

    fix move sessions when move service

--- application/views/admin/legalservices/cases/link_service.php
+++ application/views/admin/legalservices/cases/link_service.php
@@ -7,7 +7,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                        aria-hidden="true">&times;</span></button>
+                            aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title">
                     <?php echo _l('link_service'); ?>
                 </h4>
@@ -15,27 +15,26 @@
             <div class="modal-body">
                 <div class="row">
                     <div class="col-md-12">
-                        <?php 
-                          $this->db->where('show_on_sidebar', 1);
-                          $services = $this->db->get(db_prefix() . 'my_basic_services')->result_array();
-                            $child_linked_services = $this->db->get_where(db_prefix() . 'my_link_services',['service_id'=>$ServID,'rel_id'=>(isset($project) ? $project->id : '')])->result();
-                            foreach ($child_linked_services as $child_linked_service){
-                                foreach ($services as $key=>$service){
-                                    if($child_linked_service->to_service_id == $service['id'])unset($services[$key]);
-                                }
+                        <?php
+                        $this->db->where('show_on_sidebar', 1);
+                        $services = $this->db->get(db_prefix() . 'my_basic_services')->result_array();
+                        $child_linked_services = $this->db->get_where(db_prefix() . 'my_link_services',['service_id'=>$ServID,'rel_id'=>(isset($project) ? $project->id : '')])->result();
+                        foreach ($child_linked_services as $child_linked_service){
+                            foreach ($services as $key=>$service){
+                                if($child_linked_service->to_service_id == $service['id'])unset($services[$key]);
                             }
-//                        var_dump($services);
+                        }
                         ?>
                         <div class="copy-project-tasks-status-wrapper">
                             <p class="bold"><?php echo _l('LegalService'); ?></p>
                             <?php foreach($services as $service){?>
-                            <div class="radio radio-primary">
-                                <input type="radio" name="service_id" value="<?php echo $service['id']; ?>"
-                                    id="service_id<?php echo $service['id']; ?>"
-                                    <?php if($service['id'] == '1'){echo ' checked';} ?>>
-                                <label
-                                    for="service_id<?php echo $service['id']; ?>"><?php echo $service['name']; ?></label>
-                            </div>
+                                <div class="radio radio-primary">
+                                    <input type="radio" name="service_id" value="<?php echo $service['id']; ?>"
+                                           id="service_id<?php echo $service['id']; ?>"
+                                        <?php if($service['id'] == '1'){echo ' checked';} ?>>
+                                    <label
+                                            for="service_id<?php echo $service['id']; ?>"><?php echo $service['name']; ?></label>
+                                </div>
                             <?php } ?>
                             <hr />
                         </div>
@@ -45,21 +44,21 @@
                         </div>
                         <div class="checkbox checkbox-primary mleft10 link_tasks-copy-option">
                             <input type="checkbox" name="tasks_include_checklist_items"
-                                id="link_tasks_include_checklist_items" checked>
+                                   id="link_tasks_include_checklist_items" checked>
                             <label
-                                for="link_tasks_include_checklist_items"><small><?php echo _l('copy_project_task_include_check_list_items'); ?></small></label>
+                                    for="link_tasks_include_checklist_items"><small><?php echo _l('copy_project_task_include_check_list_items'); ?></small></label>
                         </div>
                         <div class="checkbox checkbox-primary mleft10 link_tasks-copy-option">
                             <input type="checkbox" name="task_include_assignees" id="link_task_include_assignees"
-                                checked>
+                                   checked>
                             <label
-                                for="link_task_include_assignees"><small><?php echo _l('copy_project_task_include_assignees'); ?></small></label>
+                                    for="link_task_include_assignees"><small><?php echo _l('copy_project_task_include_assignees'); ?></small></label>
                         </div>
                         <div class="checkbox checkbox-primary mleft10 link_tasks-copy-option">
                             <input type="checkbox" name="task_include_followers"
-                                id="copy_project_link_task_include_followers" checked>
+                                   id="copy_project_link_task_include_followers" checked>
                             <label
-                                for="copy_project_link_task_include_followers"><small><?php echo _l('copy_project_task_include_followers'); ?></small></label>
+                                    for="copy_project_link_task_include_followers"><small><?php echo _l('copy_project_task_include_followers'); ?></small></label>
                         </div>
                         <div class="checkbox checkbox-primary">
                             <input type="checkbox" name="milestones" id="link_c_milestones" checked>
@@ -73,35 +72,41 @@
                             <input type="checkbox" name="files" id="link_c_files" class="copy" checked>
                             <label for="link_c_files"><?php echo _l('project_files'); ?></label>
                         </div>
+                        <?php if($ServID == 1 || $ServID == 22){?>
+                            <div class="checkbox checkbox-primary">
+                                <input type="checkbox" class="copy" name="sessions" id="c_link_sessions" checked>
+                                <label for="c_link_sessions"><?php echo _l('sessions'); ?></label>
+                            </div>
+                        <?php }?>
                         <hr />
                         <div class="copy-project-link_tasks-status-wrapper">
                             <p class="bold"><?php echo _l('copy_project_tasks_status'); ?></p>
                             <?php foreach($task_statuses as $cp_link_task_status){ ?>
-                            <div class="radio radio-primary">
-                                <input type="radio" name="copy_project_task_status"
-                                    value="<?php echo $cp_link_task_status['id']; ?>"
-                                    id="cp_link_task_status_<?php echo $cp_link_task_status['id']; ?>"
-                                    <?php if($cp_link_task_status['id'] == '1'){echo ' checked';} ?>>
-                                <label
-                                    for="cp_link_task_status_<?php echo $cp_link_task_status['id']; ?>"><?php echo $cp_link_task_status['name']; ?></label>
-                            </div>
+                                <div class="radio radio-primary">
+                                    <input type="radio" name="copy_project_task_status"
+                                           value="<?php echo $cp_link_task_status['id']; ?>"
+                                           id="cp_link_task_status_<?php echo $cp_link_task_status['id']; ?>"
+                                        <?php if($cp_link_task_status['id'] == '1'){echo ' checked';} ?>>
+                                    <label
+                                            for="cp_link_task_status_<?php echo $cp_link_task_status['id']; ?>"><?php echo $cp_link_task_status['name']; ?></label>
+                                </div>
                             <?php } ?>
                             <hr />
                         </div>
                         <div class="form-group">
                             <label for="clientid_copy_project"><?php echo _l('project_customer'); ?></label>
                             <select id="clientid" name="clientid_copy_project" data-live-search="true" data-width="100%"
-                                class="ajax-search"
-                                data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
+                                    class="ajax-search"
+                                    data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
                                 <?php $selected = (isset($project) ? $project->clientid : '');
-                            if($selected == ''){
-                                $selected = (isset($project) ? $project->clientid : '');
-                            }
-                            if ($selected != '') {
-                                $rel_data = get_relation_data('customer', $selected);
-                                $rel_val = get_relation_values($rel_data, 'customer');
-                                echo '<option value="' . $rel_val['id'] . '" selected>' . $rel_val['name'] . '</option>';
-                            } ?>
+                                if($selected == ''){
+                                    $selected = (isset($project) ? $project->clientid : '');
+                                }
+                                if ($selected != '') {
+                                    $rel_data = get_relation_data('customer', $selected);
+                                    $rel_val = get_relation_values($rel_data, 'customer');
+                                    echo '<option value="' . $rel_val['id'] . '" selected>' . $rel_val['name'] . '</option>';
+                                } ?>
                             </select>
                         </div>
                         <div class="row">
@@ -118,8 +123,8 @@
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo _l('close'); ?></button>
                 <button type="submit" data-form="#link_form" autocomplete="off"
-                    data-loading-text="<?php echo _l('wait_text'); ?>"
-                    class="btn btn-info"><?php echo _l('link_service'); ?></button>
+                        data-loading-text="<?php echo _l('wait_text'); ?>"
+                        class="btn btn-info"><?php echo _l('link_service'); ?></button>
             </div>
         </div>
         <!-- /.modal-content -->
@@ -130,60 +135,60 @@
 <!-- /.modal -->
 <!-- Copy Project end -->
 <script>
-// Copy project modal and set url if ID is passed manually eq from project list area
-function link_service(id) {
+    // Copy project modal and set url if ID is passed manually eq from project list area
+    function link_service(id) {
 
-    $('#link_service').modal('show');
+        $('#link_service').modal('show');
 
-    if (typeof(id) != 'undefined') {
-        $('#link_form').attr('action', $('#link_form').data('link-url') + id);
-    }
-
-    appValidateForm($('#link_form'), {
-        start_date: 'required',
-        clientid_copy_project: 'required',
-    });
-
-    var copy_members = $('#link_c_members');
-    var copy_link_tasks = $('input[name="tasks"].copy');
-    var copy_assignees_and_followers = $(
-        'input[name="link_task_include_assignees"],input[name="link_task_include_followers"]');
-
-    copy_members.off('change');
-    copy_link_tasks.off('change');
-    copy_assignees_and_followers.off('change');
-
-    copy_members.on('change', function() {
-        if (!$(this).prop('checked')) {
-            copy_assignees_and_followers.prop('checked', false)
+        if (typeof(id) != 'undefined') {
+            $('#link_form').attr('action', $('#link_form').data('link-url') + id);
         }
-    });
 
-    copy_link_tasks.on('change', function() {
-        var checked = $(this).prop('checked');
-        if (checked) {
+        appValidateForm($('#link_form'), {
+            start_date: 'required',
+            clientid_copy_project: 'required',
+        });
 
-            var copy_assignees = $('input[name="link_task_include_assignees"]').prop('checked');
-            var copy_followers = $('input[name="link_task_include_followers"]').prop('checked');
+        var copy_members = $('#link_c_members');
+        var copy_link_tasks = $('input[name="tasks"].copy');
+        var copy_assignees_and_followers = $(
+            'input[name="link_task_include_assignees"],input[name="link_task_include_followers"]');
 
-            if (copy_assignees || copy_followers) {
+        copy_members.off('change');
+        copy_link_tasks.off('change');
+        copy_assignees_and_followers.off('change');
+
+        copy_members.on('change', function() {
+            if (!$(this).prop('checked')) {
+                copy_assignees_and_followers.prop('checked', false)
+            }
+        });
+
+        copy_link_tasks.on('change', function() {
+            var checked = $(this).prop('checked');
+            if (checked) {
+
+                var copy_assignees = $('input[name="link_task_include_assignees"]').prop('checked');
+                var copy_followers = $('input[name="link_task_include_followers"]').prop('checked');
+
+                if (copy_assignees || copy_followers) {
+                    $('input[name="members"].copy').prop('checked', true);
+                }
+
+                $('.copy-project-link_tasks-status-wrapper').removeClass('hide');
+                $('.link_tasks-copy-option').removeClass('hide');
+
+            } else {
+                $('.copy-project-link_tasks-status-wrapper').addClass('hide');
+                $('.link_tasks-copy-option').addClass('hide');
+            }
+        });
+
+        copy_assignees_and_followers.on('change', function() {
+            var checked = $(this).prop('checked');
+            if (checked == true) {
                 $('input[name="members"].copy').prop('checked', true);
             }
-
-            $('.copy-project-link_tasks-status-wrapper').removeClass('hide');
-            $('.link_tasks-copy-option').removeClass('hide');
-
-        } else {
-            $('.copy-project-link_tasks-status-wrapper').addClass('hide');
-            $('.link_tasks-copy-option').addClass('hide');
-        }
-    });
-
-    copy_assignees_and_followers.on('change', function() {
-        var checked = $(this).prop('checked');
-        if (checked == true) {
-            $('input[name="members"].copy').prop('checked', true);
-        }
-    });
-}
+        });
+    }
 </script>

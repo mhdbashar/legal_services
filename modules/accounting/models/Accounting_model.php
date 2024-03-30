a2c4ff48c970b87b6418b1e476cdca89d79b6f4f
Hiba Kharma <hibakharma@gmail.com>
2024-03-30 20:18:40 +0800

    fix again

--- modules/accounting/models/Accounting_model.php
+++ modules/accounting/models/Accounting_model.php
@@ -20673,7 +20673,7 @@ class Accounting_model extends App_Model
      * @return boolean
      */
     public function automatic_credit_note_conversion($data){
-       
+
         $this->delete_convert($data['credit_note_id'], 'credit_note');
 
         $payment_account = get_option('acc_credit_note_payment_account');
